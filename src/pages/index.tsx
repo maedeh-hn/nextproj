f884b52fddeb3f40f7be2a601447bf717f3e6c70
unknown <smartapply.it7@gmail.com>
2022-10-25 16:54:55 -0700

    start

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,8 +5,10 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-[red]">
-lllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllllll
+    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2">
+      <div className="bg-blue-200 hidden md:block md:row-span-2 md-span-3">category desktop</div>
+      <div className="bg-blue-200 hidden md:block md:col-span-9">sort desktop</div>
+      <div className="bg-blue-200 md:col-span-9">blogs</div>
     </div>
   )
 }
