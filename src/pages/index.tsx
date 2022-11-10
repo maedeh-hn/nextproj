b3dd70db5a22cc273bc5b01ef487447d1b2ce0f2
eskandari <maedeh.vega1993@gmail.com>
2022-11-10 12:47:21 +0330

    befor pull

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from 'next'
+import {ChevronDoenIcon} from "@heroicons/react/outline"
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2">
-      <div className="bg-blue-200 hidden md:block md:row-span-2 md-span-3">category desktop</div>
-      <div className="bg-blue-200 hidden md:block md:col-span-9">sort desktop</div>
-      <div className="bg-blue-200 md:col-span-9">blogs</div>
+    <div className="grid gap-4 md:grid-cols-12 md:grid-rows-2 w-full">
+      <div className="bg-[aliceblue] hidden md:block md:row-span-2 md:col-span-3">
+        <div>
+          <div>دسته بندی مقالات</div>
+          <div></div>
+        </div>
+      </div>
+      <div className="bg-[aliceblue] hidden md:block md:col-span-9">sort desktop</div>
+      <div className="bg-[aliceblue] md:col-span-9">blogs</div>
     </div>
   )
 }
